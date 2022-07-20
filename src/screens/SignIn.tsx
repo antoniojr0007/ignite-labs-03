982bb8f4ccf6f6eae0b280afc9b664b0de60af9b
ANTONIO FERREIRA DE OLIVEIRA JUNIOR <antonio-250291@hotmail.com>
2022-07-19 22:55:30 -0300

    Feat: completed signIn Screen and components input e buuton

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,36 @@
-import { Heading, VStack } from 'native-base'
-import React from 'react'
+import { Heading, Icon, useTheme, VStack } from 'native-base'
+import { Envelope, Key } from 'phosphor-react-native'
+import React, { useState } from 'react'
+import Logo from '../assets/logo_primary.svg'
+import { Button } from '../components/Button'
+import { Input } from '../components/Input'
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { colors  } = useTheme() 
+  
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
-        Acesse sua conta
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={8}>
+        Acesse sua conta 
       </Heading>
+      <Input 
+        mb={4}
+        placeholder="Email"
+        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={4}/>}
+        onChangeText={setEmail}
+      />
+      <Input 
+        mb={8}
+        placeholder="Senha"
+        InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={4}/>}
+        secureTextEntry
+        onChangeText={setPassword}
+      />
+      <Button title='Entrar' w='full' />
     </VStack>
   )
 }
